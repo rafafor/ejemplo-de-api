c6db810701935e5050c6959d38a8ffc41d72343b
Rafael <rafafor@gmail.com>
2017-01-10 17:06:42 +0100

    Primera version API

---
+++ app/server.js
@@ -0,0 +1,13 @@
+//llamamos a Express
+var express = require('express') 
+var app = express()               
+// establecemos nuestro puerto
+var port = process.env.PORT || 8080  
+
+
+app.get('/', function(req, res) {
+  res.json({ mensaje: '¡Hola Mundo!' })   
+})
+
+app.listen(port)
+console.log('API escuchando en el puerto ' + port)
