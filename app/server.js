a12b8a2bb15e50d15633621a5012bcff43112abe
Rafael <rafafor@gmail.com>
2017-01-10 20:50:52 +0100

    Cervezas con mongodb

--- app/server.js
+++ app/server.js
@@ -1,13 +1,18 @@
-//llamamos a Express
-var express = require('express') 
-var app = express()               
-// establecemos nuestro puerto
-var port = process.env.PORT || 8080  
+var express = require('express') //llamamos a Express
+require('./db')
+var app = express()       
+var bodyParser = require('body-parser')        
 
+var port = process.env.PORT || 8080  // establecemos nuestro puerto
 
-app.get('/', function(req, res) {
-  res.json({ mensaje: '¡Hola Mundo!' })   
-})
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())            
 
+// nuestra ruta irá en http://localhost:8080/api
+// es bueno que haya un prefijo, sobre todo por el tema de versiones de la API
+var router = require('./routes')
+app.use('/api', router)
+
+//arrancamos el servidor
 app.listen(port)
 console.log('API escuchando en el puerto ' + port)
